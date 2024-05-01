5d7d1158edd4eb6115c54ead4dace7eef97962dd
Gregory Spain <gregoryspaindev@gmail.com>
2024-04-30 23:57:01 -0400

    mobile nav looks good

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,28 +5,48 @@ import HeroImage from '../HeroImage/HeroImage.js';
 import logo from '../../assets/FinalLogo.svg'
 import DesktopNavigation from '../DesktopHeader/DesktopNavigation.js';
 import MobileNavigation from '../MobileNavigation/MobileNavigation.js';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { useState } from 'react';
 
 
 function Header(props) {
 
-  const [showHome, showDesign, showDev, showPrevExp] = props.handlers
+  const [showModal, setShowModal] = useState(false);
+  const [showHome, showDesign, showDev, showPrevExp] = props.handlers;
 
   const NavigationHandlers = [showDesign, showDev, showPrevExp];
 
   const { width } = useWindowDimensions();
-  const pageHeader = ["Design Projects",
-                      "Development Projects",
-                      "Previous Experience",
-                      "Hi, I'm"
-                      ]
+
+  const pageHeader =
+    [
+      "Design Projects",
+      "Development Projects",
+      "Previous Experience",
+      "Hi, I'm"
+    ];
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  }
+  
+  const hideModalHandler = () => {
+    setShowModal(false);
+  }
 
   return (
     <header className='header'>
       <div className='header_container'>
-        <div className='logo_container'>
+      {!showModal && width < 1000 ? <div></div> : <MobileNavigation hideModal={hideModalHandler} handlers={NavigationHandlers} pageIndex={props.pageIndex}/>}
+        {!showModal && <div className='logo_container'>
           <img src={ logo } alt='logo for gs design and development' onClick={showHome}/>
-        </div>
-        {width < 1000 ? <MobileNavigation handlers={NavigationHandlers} pageIndex={props.pageIndex}/> :
+        </div>}
+        {width < 1000 ?
+          <div className='menu_icon'>
+            {showModal ? <CloseIcon fontSize='large' onClick={hideModalHandler} /> : <MenuIcon fontSize='large' onClick={showModalHandler} />}
+          </div> :
           <DesktopNavigation 
             handlers={NavigationHandlers}
             pageIndex={props.pageIndex}
--- src/components/MobileNavigation/MobileNavigation.js
+++ src/components/MobileNavigation/MobileNavigation.js
@@ -1,23 +1,19 @@
-import { Fragment } from 'react';
 import './MobileNavigation.css'
 
-import MenuIcon from '@mui/icons-material/Menu';
 
 function MobileNavigation(props) {
 
 const [showDesign, showDev, showPrevExp] = props.handlers
 
   return (
-    <Fragment>
-    <MenuIcon />
+      
     <div className='mobile_nav_container'>
     <ul className='mobile_nav_list'>
-      <li><input type='button' className='mobile_nav_design' onClick={showDesign} value={'Design'} style={{color: props.pageIndex === 0 && "#ff3399"}}/></li>
-      <li><input type='button' className='mobile_nav_dev' onClick={showDev} value={'Development'} style={{color: props.pageIndex === 1 && "#ff3399"}}/></li>
-      <li><input type='button' className='mobile_nav_prev' onClick={showPrevExp} value={'Previous Experience'} style={{color: props.pageIndex === 2 && "#ff3399"}}/></li>
+      <li><input type='button' className='mobile_nav_design' onClick={() => {props.hideModal(); showDesign()}} value={'Design'} style={{color: props.pageIndex === 0 && "#ff3399"}}/></li>
+      <li><input type='button' className='mobile_nav_dev' onClick={() => {props.hideModal(); showDev()}} value={'Development'} style={{color: props.pageIndex === 1 && "#ff3399"}}/></li>
+      <li><input type='button' className='mobile_nav_prev' onClick={() => {props.hideModal(); showPrevExp()}} value={'Previous Experience'} style={{color: props.pageIndex === 2 && "#ff3399"}}/></li>
     </ul>
   </div>
-  </Fragment>
   )
 }
 
